d212e2bc84c39a48a739d2e160cc562231accf9e
Purushotam Sinha <purushotam.sinha@cogentinfo.com>
2020-07-08 09:12:55 -0400

    Add Java Files

---
+++ JavaWorld/src/com/java/JavaWorld.java
@@ -0,0 +1,12 @@
+package com.java;
+
+public class JavaWorld {
+
+	public static void main(String[] args) {
+		System.out.println("JavaWorld.main()");
+		System.out.println("Welcome to JavaWorld Mr."+args[0]);
+		System.out.println("Finished");
+
+	}
+
+}
